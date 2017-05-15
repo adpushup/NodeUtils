d3f0fdfaddc7bcf15496ce62af58a36fa2eb0976
Dhiraj Singh <dhiraj@adpushup.com>
2017-05-15 23:23:28 +0530

    Added couchabse basic functions to library.

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const utils = require('./src/utils'),
 	promiseForeach = require('./src/promiseForeach'),
 	promiseWhile = require('./src/promiseWhile'),
+	couchbaseService = require('./src/couchbase/index'),
 	adsenseService = require('./src/adsenseApi/adsenseService');
 
 module.exports = {
 	utils,
 	promiseForeach,
 	promiseWhile,
-	adsenseService
+	adsenseService,
+	couchbaseService
 }
---
+++ src/couchbase/index.js
@@ -0,0 +1,49 @@
+const couchbase = require('couchbase'),
+	Promise = require('bluebird'),
+	N1qlQuery = couchbase.N1qlQuery,
+	connectedBuckets = {};
+
+module.exports = (host, bucket, bucketPassword) => {
+	const cluster = new couchbase.Cluster(host),
+		connect = () => {
+			return new Promise((resolve, reject) => {
+				if (connectedBuckets[bucket]) {
+					return resolve(connectedBuckets[bucket]);
+				}
+				connectedBuckets[bucket] = cluster.openBucket(bucket, bucketPassword, (err) => {
+					if (err) {
+						return reject(err);
+					}
+					connectedBuckets[bucket] = Promise.promisifyAll(bucket);
+					return resolve(connectedBuckets[bucket]);
+				});
+			});
+		};
+
+	return {
+		queryDB = (query) => {
+			return connect()
+				.then((bucket) => bucket.queryAsync(N1qlQuery.fromString(query)))
+		},
+		getDoc: (docId) => {
+			return connect()
+				.then(() => bucket.getAsync(docId))
+				.then(res => res);
+		},
+		createDoc: (key, json, option) => {
+			return connect()
+				.then(() => {
+					json.dateCreated = +new Date();
+					return bucket.insertAsync(key, json, option);
+				});
+		},
+		updateDoc: function (docId, doc, cas) {
+			if (cas) {
+				return connect()
+					.then(() => bucket.replaceAsync(docId, doc, { cas: cas }));
+			}
+			return connect()
+				.then(() => bucket.upsertAsync(docId, doc));
+		}
+	};
+};
