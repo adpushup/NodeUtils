5e5056cd943d3c9573e733cb8bfe48b52e70db7a
amitdasgupta <amitdsgupta007@gmail.com>
2020-08-21 16:37:01 +0530

    Added prettier configuration, fixed bug of bucket undefined in ApiObject , added test case for connection established, added few test checks

--- src/tests/couchbase.test.js
+++ src/tests/couchbase.test.js
@@ -1,76 +1,64 @@
-const cbWrapper = require("../couchbase");
-const connection = require("../couchbase/connection");
-const api = require("../couchbase/api");
-const config = require("../config/config");
+const cbWrapper = require('../couchbase');
+const connection = require('../couchbase/connection');
+const api = require('../couchbase/api');
+const config = require('../config/config');
 const {
-  isObject,
-  containsProperty,
-  areAllPropertyFunc,
-} = require("./testChecks");
+	isObject,
+	containsProperty,
+	areAllPropertyFunc,
+	isNotEmptyObject,
+} = require('./testChecks');
 
-const getMockObject = (timer) => {
-  const { cluster, couchbase } = connection(
-    "couchbase://" + config.couchBase.HOST,
-    config.couchBase.DEFAULT_USER_NAME,
-    config.couchBase.DEFAULT_USER_PASSWORD
-  );
+const getMockObject = (delay) => {
+	const { cluster, couchbase } = connection(
+		'couchbase://' + config.couchBase.HOST,
+		config.couchBase.DEFAULT_USER_NAME,
+		config.couchBase.DEFAULT_USER_PASSWORD
+	);
 
-  const _origOpenBucket = cluster.openBucket;
-  cluster.openBucket = function (bucket, callback) {
-    if (timer) timer();
-    return _origOpenBucket.call(cluster, bucket, callback);
-  };
+	const _origOpenBucket = cluster.openBucket;
+	cluster.openBucket = function (bucket, callback) {
+		if (delay) setTimeout(delay);
+		return _origOpenBucket.call(cluster, bucket, callback);
+	};
 
-  //from this api object we can create timer,check callback object is function,check different api poiting to same API object,All Api queries should be executed at a single time when promise is resolved.
-  const apiInst = api({
-    bucket: config.couchBase.DEFAULT_BUCKET,
-    cluster,
-    couchbase,
-  });
-  return apiInst;
+	//from this api object we can create timer,check callback object is function,check different api poiting to same API object,All Api queries should be executed at a single time when promise is resolved.
+	const apiInst = api({
+		bucket: config.couchBase.DEFAULT_BUCKET,
+		cluster,
+		couchbase,
+	});
+	return apiInst;
 };
 
-test("cbWrapper returns API Object", async () => {
-  const connectedObj = await cbWrapper(
-    "couchbase://" + config.couchBase.HOST,
-    config.couchBase.DEFAULT_BUCKET,
-    config.couchBase.DEFAULT_USER_NAME,
-    config.couchBase.DEFAULT_USER_PASSWORD
-  );
-  expect(isObject(connectedObj)).toBe(true);
-  // const { queryDB } = connectedObj;
-  // const result = await queryDB(testingQuery);
-  // console.log(result, "---------------->1");
-  // also test specific methods
+test('cbWrapper returns API Object', async () => {
+	const connectedObj = await cbWrapper(
+		'couchbase://' + config.couchBase.HOST,
+		config.couchBase.DEFAULT_BUCKET,
+		config.couchBase.DEFAULT_USER_NAME,
+		config.couchBase.DEFAULT_USER_PASSWORD
+	);
+	expect(isObject(connectedObj)).toBe(true);
 });
 
 // path connection object to mock couchbase connection behaviour  "mock couchbase connection"
-test("is mock object valid", () => {
-  const apiInst = getMockObject();
-  expect(isObject(apiInst)).toBe(true);
-  expect(
-    containsProperty(
-      apiInst,
-      "queryDB",
-      "getDoc",
-      "createDoc",
-      "updateDoc",
-      "getCouchBaseObj"
-    )
-  ).toBe(true);
-  expect(areAllPropertyFunc(apiInst)).toBe(true);
+test('is mock object valid', () => {
+	const apiInst = getMockObject();
+	expect(isObject(apiInst)).toBe(true);
+	expect(
+		containsProperty(apiInst, 'queryDB', 'getDoc', 'createDoc', 'updateDoc', 'getCouchBaseObj')
+	).toBe(true);
+	expect(areAllPropertyFunc(apiInst)).toBe(true);
 });
 
 //test is connected to couchbase
-test("is connected to couchbase", async () => {
-  try {
-    const apiInst = getMockObject();
-    // const { queryDB } = apiInst;
-    // const result = await queryDB(testingQuery);
-    // console.log(result, "---------------->2");
-    const { getDoc } = apiInst;
-    const result = await getDoc("site::40792");
-  } catch (error) {
-    console.log(`error:${error}`);
-  }
+test('is connected to couchbase', async () => {
+	try {
+		const apiInst = getMockObject();
+		const { getDoc } = apiInst;
+		const result = await getDoc('site::40792');
+		expect(isNotEmptyObject(result)).toBe(true);
+	} catch (error) {
+		console.log(`error:${error}`);
+	}
 });
--- src/tests/testChecks/index.js
+++ src/tests/testChecks/index.js
@@ -1,28 +1,33 @@
-const _ = require("lodash");
+const _ = require('lodash');
 
 const isObject = (obj) => {
-  if (_.isArray(obj) || !obj) return false;
-  return _.isObject(obj);
+	if (_.isArray(obj) || !obj) return false;
+	return _.isObject(obj);
+};
+
+const isNotEmptyObject = (obj) => {
+	return !_.isEmpty(obj);
 };
 
 const containsProperty = (obj, ...args) => {
-  if (!isObject(obj)) return false;
-  for (const arg of args) {
-    if (!_.has(obj, [arg])) return false;
-  }
-  return true;
+	if (!isObject(obj)) return false;
+	for (const arg of args) {
+		if (!_.has(obj, [arg])) return false;
+	}
+	return true;
 };
 
 const areAllPropertyFunc = (obj, ...args) => {
-  if (!isObject(obj)) return false;
-  for (const arg of args) {
-    if (!_.isFunction(obj[arg])) return false;
-  }
-  return true;
+	if (!isObject(obj)) return false;
+	for (const arg of args) {
+		if (!_.isFunction(obj[arg])) return false;
+	}
+	return true;
 };
 
 module.exports = {
-  isObject,
-  containsProperty,
-  areAllPropertyFunc,
+	isObject,
+	containsProperty,
+	areAllPropertyFunc,
+	isNotEmptyObject,
 };
