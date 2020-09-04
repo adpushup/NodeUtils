cbc54f18e3260a04e3148e8006c3291c2a6926e1
AnilAdPushup <anil.panghal@adpushup.com>
2020-09-04 18:53:31 +0530

    removed googleApis from dependecies

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const utils = require('./src/utils'),
 	promiseForeach = require('./src/promiseForeach'),
 	promiseWhile = require('./src/promiseWhile'),
 	couchbaseService = require('./src/couchbase/index'),
-	mailService = require('./src/mailService/index'),
-	adsenseService = require('./src/adsenseApi/adsenseService');
+	mailService = require('./src/mailService/index');
+//adsenseService = require('./src/adsenseApi/adsenseService');
 
 module.exports = {
 	utils,
 	promiseForeach,
 	promiseWhile,
-	adsenseService,
+	//adsenseService,
 	couchbaseService,
 	mailService
 }
